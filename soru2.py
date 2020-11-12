c2045e8d76c0dc1fe9453dac116020ba21e00b32
DeytaPau <68224244+DeytaPau@users.noreply.github.com>
2020-11-12 21:15:19 +0300

    create soru2.py

---
+++ soru2.py
@@ -0,0 +1,12 @@
+import numpy as np 
+  
+giris_array = np.array([[1, 2, 3], [2, 4, 5]]) 
+  
+
+print("girdi: \n", str(giris_array)) 
+  
+
+kolon = giris_array.flatten() 
+
+
+print("\nKolon'a donusturulmus hali:  ", kolon) 
